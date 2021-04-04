cf3518bb5fc1bb167f251351fe2cb57215c90123
ArtthurAlarcon <78834437+ArtthurAlarcon@users.noreply.github.com>
2021-04-04 05:09:14 -0500

    Add files via upload

---
+++ registry-service/src/test/java/com/innova/movies/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innova.movies.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
